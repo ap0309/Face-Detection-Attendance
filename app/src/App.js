50e27d79511a085ece1689ea458d0abf6c4c324e
AyushPatel_03 <ap090703@gmail.com>
2024-03-08 19:36:15 +0530

    latest commit

--- app/src/App.js
+++ app/src/App.js
@@ -10,16 +10,30 @@ function App() {
   return (
     <>
       <Navbar />
+
       <div className="eventheader row row-cols-1 row-cols-md-3 g-4 justify-content-center align-items-center">
         <div className="col text-center mb-3 mb-md-0">
-          <Card src={reguser} title="Register New User" btnctn="Click Here" />
+          <Card
+            src={reguser}
+            title="Register New User"
+            btnctn="Click Here"
+          />
         </div>
+
         <div className="col text-center mb-3 mb-md-0">
-          <Card src={markp} title="Mark Attendance Here" btnctn="Click Here" />
+          <Card
+            src={markp}
+            title="Mark Attendance Here"
+            btnctn="Click Here" />
         </div>
+
         <div className="col text-center">
-          <Card src={db} title="Access Database" btnctn="Click Here" />
+          <Card
+            src={db}
+            title="Access Database" b
+            btnctn="Click Here" />
         </div>
+
       </div>
     </>
   );
